049c8f34744a3ae2aa4d065b6e6520a94d09e094
Dave Ward <professorx00@gmail.com>
2019-09-14 20:12:30 -0400

    Update the game to have lose and win state

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-const WordCreator = require("./word");
-const axios = require("axios");
+const WordCreator = require("./word.js");
 require('dotenv').config();
 const randomWord = require("./randomWords.js")
+const inquirer = require("inquirer")
+
+let turn=0;
+
+let word = new WordCreator(randomWord());
+word.printWord();
+
+function getGuess(){
+    return inquirer.prompt([
+    {
+        "message": "Please enter a letter to guess",
+        "type": "input",
+        "name": "guess",
+        validate:function(value){
+            if(value.length>1){
+                return false
+            }
+            return true
+        }
+    }]).then((answer)=>{
+        
+        word.guess(answer.guess)
+        console.log(`Turn: ${word.turn}`)
+        word.checkSolution()
+        if(word.isWordSolved){
+            console.log("You Won")
+        }
+        else{
+            if(word.turn>15){
+                let correctWord = []
+                word.word.forEach((element=>{
+                    correctWord.push(element.letter)
+                }))
+                console.log("You Lose! You are out of Turns")
+                console.log(correctWord.join("") + " is the word.")
+            }
+            else{
+                startGame();
+            }
+        }
+        
+        
+    })
+}
 
 
-let word = randomWord();
-console.log(word)
+function startGame(){
+    
+    if(word.isWordSolved){
+        console.log("new game")
+    }
+    else{
+        console.log("Can you guess the word in 15 turns?")
+        getGuess()
+    }
+}
+
+startGame();
--- word.js
+++ word.js
@@ -3,10 +3,11 @@ let letter = require('./letter');
 let Word = function(word){
     this.word = [];
     this.wordString = [];
+    this.isWordSolved = false;
+    this.turn = 1;
     for(let x=0; x<word.length;x++){
         let a = new letter(word[x]);
-        this.word.push(a);
-        
+        this.word.push(a);     
     }
     for(let i=0;i<this.word.length;i++){
             this.wordString.push(this.word[i].createLetter());
@@ -16,13 +17,27 @@ let Word = function(word){
     }
     this.guess =  function(input){
         this.word.forEach((element,index) => {
+            if(element.letter === input.toLowerCase()){
+                this.turn--;
+            }
             element.checkLetter(input);
             if(element.hasBeenGuessed === true && element.letter===input.toLowerCase()){
+                console.log(`${input} was found`)
                 this.wordString.splice(index,1,input);
             }
         });
         
+        this.turn++;
         this.printWord();
+    }
+    this.checkSolution = function(){
+        let check = this.wordString.filter((letter)=> letter==="_" ? true:false)
+        if(check.length>0){
+            this.isWordSolved = false
+        }
+        else{
+            this.isWordSolved = true
+        }
     }
 }
 
